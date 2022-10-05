70fce55ebd18e452e5eb9678311e6b6f43e62f50
Misash <misash@outlook.com>
2022-10-05 00:06:29 -0500

    frst commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import logo from './media/LOGO-B.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className='LOGO-B.png'>
+      <img src={logo} alt="Logo" width="300" height="115"/>
+      </div>
+      <div className='App-login'>
+      <p>Tu sitio favorito para <br /> descargar imágenes, fotos, <br /> video y mucho más.<br />
+      <br />Todo al alcance de tu mano,<br /> solo aquí en Bluetify.</p>
+      </div> 
+      <div className='App-bottom'>
+      Copyright © 2022 Bluetify
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
