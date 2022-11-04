de190d27117f5dac8ed82d10e828b19998d12ffc
Misash <misash@outlook.com>
2022-11-04 00:14:36 -0500

    some advances

--- src/pages/InfoContenido.jsx
+++ src/pages/InfoContenido.jsx
@@ -5,55 +5,82 @@ import Col from 'react-bootstrap/Col';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+
+import { FaFileDownload } from 'react-icons/fa';
+import { GiPodium } from "react-icons/gi";
+import { AiOutlineNumber } from "react-icons/ai";
+import { FaDollarSign } from "react-icons/fa";
 
 import "../CSS/infoContenido.css"
 
 
-function InfoContenido() {
+
+
+
+function InfoContenido(props) {
+
+    //rating guarda el valor de start rating
+    const [rating, setRating] = useState(props.rating); //estrellas del contenido
+    const [hover, setHover] = useState(0);
+
     return (
         <div>
 
             <NavBar />
 
 
-            <Card calssName="info">
+            <Card className="info">
                 <Card.Header as="h5">
                     <Row>
-                        <Col> <h1>Don't Cry</h1></Col>
-                        <Col>.MP4</Col>
+                        <Col> <h1>props.titulo</h1></Col>
+                        <Col>props.extension</Col>
                     </Row>
                     <Row>
-                        <Col><h3>Guns N' Roses</h3></Col>
+                        <Col><h3>props.autor</h3></Col> 
                     </Row>
                 </Card.Header>
                 <Card.Body>
                     <Row>
-                        <Col>********</Col>
-                        <Col> -- 32</Col>
+                        <Col>
+                            {/* START RATING */}
+                            <div className="star-rating">
+                                {[...Array(5)].map((star, index) => {
+                                    index += 1;
+                                    return (
+                                        <button
+                                            type="button"
+                                            key={index}
+                                            className={index <= (hover || rating) ? "on" : "off"}
+                                        // onClick={() => setRating(index)}
+                                        // onMouseEnter={() => setHover(index)}
+                                        // onMouseLeave={() => setHover(rating)}
+                                        >
+                                            <span className="star">&#9733;</span>
+                                        </button>
+                                    );
+                                })}
+                            </div>
+                        </Col>
+                        <Col> <FaFileDownload/> props.numero_descargas</Col> 
                     </Row>
+                    <br />
                     <Row>
                         <Col>
-                            <p>#10  #6 Descargas</p>
-                            <p>#3  #10 Descargas</p>
-                            <h4> Descripcion:</h4>
-                            <p> Una canción perteneciente al
-                                grupo Guns N' Roses, la primera canción escrita del grupo</p>
-                            <p> $ 20</p>
+                            <p className="rankings"> <AiOutlineNumber/> props.ranking_descargas  <span className="line"> <AiOutlineNumber/>  props.ranking_descargas_anterior</span> Descargas</p>
+                            <p className="rankings"> <AiOutlineNumber/> props.ranking_nota  <span className="line"> <AiOutlineNumber/>  props.ranking_nota_anterior</span> Calificaciones</p>
+                            <h3> Descripcion:</h3>
+                            <p> props.descripcion</p>
+                            <p> <FaDollarSign/> props.precio</p>
                         </Col>
                         <Col>
                             <h4>Lista Generacional</h4>
-                            <div className="overflow-scroll" style={{ maxHeight: "200px" }}  >
-                                <p> #HardRock</p>
-                                <p> #HardRock</p>
-                                <p> #HardRock</p>
-                                <p> #HardRock</p>
-                                <p> #HardRock</p>
-                                <p> #HardRock</p>
-
+                            <div className="overflow-scroll" style={{ maxHeight: "400px" }}  >
+                                <p> props.categorias </p>
                             </div>
                         </Col>
                     </Row>
-                    <br/>
+                    <br />
                     <Row>
                         <Col>
                             <Button variant="primary">Comprar</Button>
@@ -80,38 +107,3 @@ export default InfoContenido
 
 
 
-{/* <Container className="info">
-<Row>
-    <Col> <h1>Don't Cry</h1></Col>
-    <Col>.MP4</Col>
-</Row>
-<Row>
-    <Col><h2>Guns N' Roses</h2></Col>
-</Row>
-<Row>
-    <Col>********</Col>
-    <Col> -- 32</Col>
-</Row>
-<Row>
-    <Col>
-        <p>#10  #6 Descargas</p>
-        <p>#3  #10 Descargas</p>
-        <h4> Descripcion:</h4>
-        <p> Una canción perteneciente al
-            grupo Guns N' Roses, la primera canción escrita del grupo</p>
-        <p> $ 20</p>
-    </Col>
-    <Col>
-        <h4>Lista Generacional</h4>
-        <div className="overflow-scroll" style={{ maxHeight: "200px" }}  >
-            <p> #HardRock</p>
-            <p> #HardRock</p>
-            <p> #HardRock</p>
-            <p> #HardRock</p>
-            <p> #HardRock</p>
-            <p> #HardRock</p>
-
-        </div>
-    </Col>
-</Row>
-</Container> */}
